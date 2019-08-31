b58892c6385376622ffbae35f26601315e2d78b1
Joshua Rodriguez <josh030596@gmail.com>
2019-08-31 23:02:35 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  puts num1 = 5
+  puts num2 = 4
+  sum = num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  puts num1 = 10 
+  puts num2 = 5
+  sum = num1 - num2
 end
 
 def division(num1, num2)
-
+  puts num1 = 50 
+  puts num2 = 2 
+  sum = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  puts num1 = 4
+  puts num2 = 30
+  sum = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  puts num1 = 34
+  puts num2 = 5
+  sum = num1 % num2 
 end
 
 def square_root(num)
-
+  puts num = 81
+  Math.sqrt(num) 
 end
