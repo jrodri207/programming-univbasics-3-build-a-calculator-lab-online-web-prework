3a417d61809ddfab19d2d69834f8e295e344da61
Joshua Rodriguez <josh030596@gmail.com>
2019-08-31 22:44:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,11 +11,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  puts num1 = 
+  puts num1 = 50 
+  puts num2 = 2 
+  sum = num1 / num2
 end
 
 def multiplication(num1, num2)
-  puts num1 * num2
+  puts num1 = 
 end
 
 def modulo(num1, num2)
